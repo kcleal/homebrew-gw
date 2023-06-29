032fe8d426bf907bc73f2828e1bb0ade831d1381
Kez Cleal <42997789+kcleal@users.noreply.github.com>
2023-06-29 20:01:54 +0100

    Update gw.rb

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -1,8 +1,8 @@
 class Gw < Formula
   desc "Browser for genomic sequencing data and variants"
   homepage "https://github.com/kcleal/gw"
-  url "https://github.com/kcleal/gw/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "61bf1be3449814442be98dc924e0b51e9fd90f03197a8d398c57c2d2b69cab6f"
+  url "https://github.com/kcleal/gw/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "30fc7957aa7ce61e495be798138e2ae7f3ed8022a5d3a1a726103c7f3a8bdc0d"
   license "MIT"
   depends_on "fontconfig"
   depends_on "glfw"
